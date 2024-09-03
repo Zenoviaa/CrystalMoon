33010cd89d6716517b3a145fe9ad628e31fdb1fc
Zenovia <calamityjewelstudios@gmail.com>
2024-09-03 17:41:09 -0400

    guh

---
+++ Registries/SoundRegistry.cs
@@ -0,0 +1,22 @@
+﻿using Terraria.Audio;
+
+namespace CrystalMoon.Registries
+{
+    internal static class SoundRegistry
+    {
+        public static string RootAssetPath => "CrystalMoon/Assets/Sounds/";
+
+        //Example how to add
+       // public static SoundStyle StormDragonLightningRain => new SoundStyle($"{RootAssetPath}StormDragon_LightingZap");
+
+
+        /*
+        public static SoundStyle BowCharge => new SoundStyle($"{RootAssetPath}BowCharge",
+            variantSuffixesStart: 1,
+            numVariants: 2);
+        public static SoundStyle BowShoot => new SoundStyle($"{RootAssetPath}BowShot",
+            variantSuffixesStart: 1,
+            numVariants: 2);
+        */
+    }
+}
