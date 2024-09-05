e83bb758f52ec310f87ef58da87f0e32298d3df7
Scolon <49799704+sscolon@users.noreply.github.com>
2024-09-05 16:44:46 -0400

    so fake

--- ExampleContent/Items/Fake.cs
+++ ExampleContent/Items/Fake.cs
@@ -1,5 +1,10 @@
 using CrystalMoon.ExampleContent.Projectiles;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -10,9 +15,23 @@ namespace CrystalMoon.ExampleContent.Items
     // https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
     public class Fake : ModItem
     {
+        private int _index;
+        private static List<int> _projectilesToShoot = new();
+
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            _projectilesToShoot = new();
+            _projectilesToShoot.Add(ModContent.ProjectileType<ExampleTrailingProjectile>());
+            _projectilesToShoot.Add(ModContent.ProjectileType<ExampleCircleExplosionProjectile>());
+            _projectilesToShoot.Add(ModContent.ProjectileType<ExampleMoonlightMagicHexProjectile>());
+            _projectilesToShoot.Add(ModContent.ProjectileType<ExampleMoonlightMagicRadianceProjectile>());
+        }
+
         // The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.CrystalMoon.hjson' file.
         public override void SetDefaults()
         {
+    
             Item.damage = 50;
             Item.DamageType = DamageClass.Melee;
             Item.width = 40;
@@ -29,12 +48,36 @@ namespace CrystalMoon.ExampleContent.Items
             Item.shootSpeed = 15;
         }
 
-        public override void AddRecipes()
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
         {
-            Recipe recipe = CreateRecipe();
-            recipe.AddIngredient(ItemID.DirtBlock, 10);
-            recipe.AddTile(TileID.WorkBenches);
-            recipe.Register();
+            base.ModifyTooltips(tooltips);
+            int typeToShoot = _projectilesToShoot[_index];
+            string name = ModContent.GetModProjectile(typeToShoot).Name;
+            TooltipLine firing = new TooltipLine(Mod, "Helper", $"Shooting: {name}");
+            tooltips.Add(firing);
+        }
+
+        public override bool AltFunctionUse(Player player)
+        {
+            return true;
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            if(player.altFunctionUse == 2)
+            {
+                _index++;
+                if (_index >= _projectilesToShoot.Count)
+                {
+                    _index = 0;
+                }
+                int typeToShoot = _projectilesToShoot[_index];
+                string name = ModContent.GetModProjectile(typeToShoot).Name;
+                CombatText.NewText(player.getRect(), Color.White, name, true);
+            }
+      
+            Projectile.NewProjectile(source, position, velocity, _projectilesToShoot[_index], damage, knockback, player.whoAmI);
+            return false;
         }
     }
 }
