986885106d899414ae24faff43de670cb6b022d5
Zenovia <calamityjewelstudios@gmail.com>
2024-09-05 14:06:49 -0400

    goob

---
+++ Visual/Particles/SparkleHexParticle.cs
@@ -0,0 +1,60 @@
+﻿using CrystalMoon.Systems.Particles;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace CrystalMoon.Visual.Particles
+{
+    internal class SparkleHexParticle : Particle
+    {
+        public int FrameWidth = 488;
+        public int FrameHeight = 422;
+        public int MaxHorizontalFrameCount = 5;
+        public int MaxVerticalFrameCount = 11;
+        public int FrameCounter = 0;
+        public int TicksPerFrame = 4;
+        public override void OnSpawn()
+        {
+            Rotation += Main.rand.NextFloat(-MathHelper.TwoPi, MathHelper.TwoPi);
+            Frame = new Rectangle(0, 0, FrameWidth, FrameHeight);
+            Scale = Main.rand.NextFloat(0.04f, 0.4f);
+        }
+
+        public override void Update()
+        {
+            Velocity *= 0.98f;
+            Rotation += 0.01f;
+            Scale *= 0.997f;
+            color *= 0.99f;
+
+            FrameCounter++;
+            if (FrameCounter >= TicksPerFrame)
+            {
+                if(Frame.X < FrameWidth * MaxHorizontalFrameCount)
+                {
+                    Frame.X += FrameWidth;
+                  
+                }
+
+                if(Frame.X >= Frame.Width * MaxHorizontalFrameCount)
+                {
+                    Frame.Y += FrameHeight;
+                }
+
+                if(Frame.Y >= Frame.Height * MaxVerticalFrameCount)
+                {
+                    active = false;
+                }
+                FrameCounter = 0;
+            }
+
+            fadeIn++;
+            if (fadeIn > 60)
+                active = false;
+        }
+    }
+}
