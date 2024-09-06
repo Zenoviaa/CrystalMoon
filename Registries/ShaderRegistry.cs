0426b2506186f0dfb3d7459cb5bfb88052b4e822
Scolon <49799704+sscolon@users.noreply.github.com>
2024-09-06 00:52:07 -0400

    phantasmal

--- Registries/ShaderRegistry.cs
+++ Registries/ShaderRegistry.cs
@@ -18,6 +18,7 @@ namespace CrystalMoon.Registries
         public static MiscShaderData MagicBloodletEffect => GameShaders.Misc["CrystalMoon:MagicBloodlet"];
         public static MiscShaderData MagicSparkleWaterEffect => GameShaders.Misc["CrystalMoon:MagicSparkleWater"];
         public static MiscShaderData MagicDeeyaEffect => GameShaders.Misc["CrystalMoon:MagicDeeya"];
+        public static MiscShaderData MagicPhantasmalEffect => GameShaders.Misc["CrystalMoon:MagicPhantasmal"];
         public static MiscShaderData CloudsShader => GameShaders.Misc["CrystalMoon:Clouds"];
         public static MiscShaderData CloudsFrontShader => GameShaders.Misc["CrystalMoon:CloudsFront"];
         public static MiscShaderData NightCloudsShader => GameShaders.Misc["CrystalMoon:NightClouds"];
@@ -53,7 +54,7 @@ namespace CrystalMoon.Registries
             RegisterMiscShader("MagicBloodlet", "PrimitivesPass");
             RegisterMiscShader("MagicSparkleWater", "PrimitivesPass");
             RegisterMiscShader("MagicDeeya", "PrimitivesPass");
-
+            RegisterMiscShader("MagicPhantasmal", "PrimitivesPass");
             var miscShader9 = new Ref<Effect>(CrystalMoon.Instance.Assets.Request<Effect>("Assets/Effects/Water", AssetRequestMode.ImmediateLoad).Value);
             Filters.Scene["CrystalMoon:Water"] = new Filter(new ScreenShaderData(miscShader9, "PrimitivesPass"), EffectPriority.VeryHigh);
             Filters.Scene["CrystalMoon:Water"].Load();
---
+++ Systems/Shaders/MagicPhantasmalShader.cs
@@ -0,0 +1,43 @@
+﻿using CrystalMoon.Registries;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
+
+namespace CrystalMoon.Systems.Shaders
+{
+    internal class MagicPhantasmalShader : BaseShader
+    {
+        private static MagicPhantasmalShader _instance;
+        public MagicPhantasmalShader()
+        {
+            Data = ShaderRegistry.MagicPhantasmalEffect;
+            PrimaryTexture = TrailRegistry.StarTrail;
+            NoiseTexture = TrailRegistry.StarTrail;
+            Speed = 5;
+            Repeats = 1;
+        }
+
+        public static MagicPhantasmalShader Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new MagicPhantasmalShader();
+                return _instance;
+            }
+        }
+
+        public Asset<Texture2D> PrimaryTexture { get; set; }
+        public Asset<Texture2D> NoiseTexture { get; set; }
+        public float Speed { get; set; }
+        public float Repeats { get; set; }
+        public override void Apply()
+        {
+            Effect.Parameters["transformMatrix"].SetValue(TrailDrawer.WorldViewPoint2);
+            Effect.Parameters["primaryTexture"].SetValue(PrimaryTexture.Value);
+            Effect.Parameters["noiseTexture"].SetValue(NoiseTexture.Value);
+            Effect.Parameters["time"].SetValue(Main.GlobalTimeWrappedHourly * Speed);
+            Effect.Parameters["repeats"].SetValue(Repeats);
+        }
+    }
+}
