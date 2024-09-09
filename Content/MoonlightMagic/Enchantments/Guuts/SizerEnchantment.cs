30de999fa7369488ca3927e0f142e803ff90dcb6
Zenovia <calamityjewelstudios@gmail.com>
2024-09-09 19:57:57 -0400

    guh

---
+++ Content/MoonlightMagic/Enchantments/Guuts/SizerEnchantment.cs
@@ -0,0 +1,76 @@
+﻿using CrystalMoon.Content.MoonlightMagic.Elements;
+using CrystalMoon.Systems.ScreenSystems;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using CrystalMoon.Registries;
+using CrystalMoon.Content.MoonlightMagic.Movements;
+using CrystalMoon.Systems.Particles;
+using CrystalMoon.Visual.Particles;
+
+namespace CrystalMoon.Content.MoonlightMagic.Enchantments.Guuts
+{
+    internal class SizerEnchantment : BaseEnchantment
+    {
+        private int _timer;
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            time = 30;
+        }
+    
+        public override void AI()
+        {
+            base.AI();
+
+            //Count up
+            _timer++;
+
+            //If greater than time then start homing, we'll just swap the movement type of the projectile
+            if (_timer == time)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Vector2 spawnPoint = Projectile.Center + Main.rand.NextVector2Circular(8, 8);
+                    Vector2 velocity = Main.rand.NextVector2Circular(8, 8);
+                  //  Particle.NewParticle<SparkleHexParticle>(spawnPoint, velocity, Color.White);
+                }
+
+                Projectile.Size *= 2;
+               
+                float damage = Projectile.damage;
+                damage *= 1.25f;
+                Projectile.damage = (int)damage;
+            }
+        }
+
+        public override float GetStaffManaModifier()
+        {
+            return 0.2f;
+        }
+
+        public override int GetElementType()
+        {
+            return ModContent.ItemType<GuutElement>();
+        }
+
+
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+
+            return true;
+        }
+
+        public override void SpecialInventoryDraw(Item item, SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            base.SpecialInventoryDraw(item, spriteBatch, position, frame, drawColor, itemColor, origin, scale);
+            DrawHelper.DrawGlowInInventory(item, spriteBatch, position, ColorUtil.GuutGray);
+        }
+    }
+}
