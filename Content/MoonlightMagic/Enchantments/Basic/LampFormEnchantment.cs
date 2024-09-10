5b44daba6fbc5a4d20152cb356df524e48ab524d
Zenovia <calamityjewelstudios@gmail.com>
2024-09-10 01:51:02 -0400

    gu

--- Content/MoonlightMagic/Enchantments/Basic/LampFormEnchantment.cs
+++ Content/MoonlightMagic/Enchantments/Basic/LampFormEnchantment.cs
@@ -13,7 +13,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.UI.Chat;
 
-namespace CrystalMoon.Content.MoonlightMagic.Enchantments.Guuts
+namespace CrystalMoon.Content.MoonlightMagic.Enchantments.Basic
 {
     internal class LampFormEnchantment : BaseEnchantment
     {
