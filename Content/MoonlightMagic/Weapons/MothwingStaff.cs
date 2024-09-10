9a837c262f4c683b609a34860f7a2b9be34545c7
Zenovia <calamityjewelstudios@gmail.com>
2024-09-10 17:03:41 -0400

    new staff

---
+++ Content/MoonlightMagic/Weapons/MothwingStaff.cs
@@ -0,0 +1,30 @@
+﻿using CrystalMoon.Content.MoonlightMagic.Forms;
+
+namespace CrystalMoon.Content.MoonlightMagic.Weapons
+{
+    internal class MothwingStaff : BaseStaff
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.damage = 9;
+            Item.shootSpeed = 13;
+            Item.useTime = 26;
+            Item.useAnimation = 26;
+            Size = 8;
+            TrailLength = 38;
+            Form = FormRegistry.Fairy.Value;
+        }
+
+
+        public override int GetNormalSlotCount()
+        {
+            return 2;
+        }
+
+        public override int GetTimedSlotCount()
+        {
+            return 1;
+        }
+    }
+}
