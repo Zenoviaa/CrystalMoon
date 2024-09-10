d3802650f6e282524f49193653040dbf10e42002
Zenovia <calamityjewelstudios@gmail.com>
2024-09-09 22:31:17 -0400

    gh

---
+++ Content/MoonlightMagic/Enchantments/Uvilis/TyphoonWaverEnchantment.cs
@@ -0,0 +1,57 @@
+﻿using CrystalMoon.Content.MoonlightMagic.Elements;
+using CrystalMoon.Systems.ScreenSystems;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using CrystalMoon.Registries;
+using CrystalMoon.Content.MoonlightMagic.Movements;
+using CrystalMoon.Systems.Particles;
+using CrystalMoon.Visual.Particles;
+
+namespace CrystalMoon.Content.MoonlightMagic.Enchantments.Uvilis
+{
+    internal class TyphoonWaverEnchantment : BaseEnchantment
+    {
+        private float _timer;
+
+        public override void AI()
+        {
+            base.AI();
+
+            //Count up
+            _timer++ ;
+            Projectile.velocity += (Projectile.velocity.SafeNormalize(Vector2.Zero) * 4).RotatedBy(MathHelper.ToRadians(15 * MathF.Sin(_timer *= 0.05f)));
+            //If greater than time then start homing, we'll just swap the movement type of the projectile
+
+        }
+
+        public override float GetStaffManaModifier()
+        {
+            return 0.3f;
+        }
+
+        public override int GetElementType()
+        {
+            return ModContent.ItemType<UvilisElement>();
+        }
+
+
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+
+            return true;
+        }
+
+        public override void SpecialInventoryDraw(Item item, SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            base.SpecialInventoryDraw(item, spriteBatch, position, frame, drawColor, itemColor, origin, scale);
+            DrawHelper.DrawGlowInInventory(item, spriteBatch, position, ColorUtil.PhantasmalGreen);
+        }
+    }
+}
---
+++ Content/MoonlightMagic/Movements/CircularMovement.cs
@@ -0,0 +1,16 @@
+﻿using CrystalMoon.Systems.MiscellaneousMath;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+
+namespace CrystalMoon.Content.MoonlightMagic.Movements
+{
+    internal class CircularMovement : BaseMovement
+    {
+       // public float maxHomingDetectDistance = 512;
+        public override void AI()
+        {
+            Projectile.velocity = Projectile.velocity.RotatedBy(MathHelper.ToRadians(15));
+        }
+    }
+}
