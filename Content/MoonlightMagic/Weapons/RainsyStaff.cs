7682793fb4e36e4cbe23dc2a1776348e06ee9db9
Zenovia <calamityjewelstudios@gmail.com>
2024-09-10 17:24:36 -0400

    goob

---
+++ Content/MoonlightMagic/Weapons/RainsyStaff.cs
@@ -0,0 +1,27 @@
+﻿using CrystalMoon.Content.MoonlightMagic.Forms;
+
+namespace CrystalMoon.Content.MoonlightMagic.Weapons
+{
+    internal class RainsyStaff : BaseStaff
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.damage = 14;
+            Item.shootSpeed = 14;
+            Item.useTime = 14;
+            Item.useAnimation = 14;
+            Size = 10;
+            TrailLength = 16;
+            Form = FormRegistry.Pickaxe.Value;
+        }
+
+
+        public override int GetNormalSlotCount()
+        {
+            return 3;
+        }
+
+
+    }
+}
