5f2d1cab07be98d391b7db73d0cce12c1557e842
Zenovia <calamityjewelstudios@gmail.com>
2024-09-11 17:59:50 -0400

    guh

---
+++ Content/MoonlightMagic/Enchantments/Hex/TrailerEnchantment.cs
@@ -0,0 +1,68 @@
+﻿using CrystalMoon.Content.MoonlightMagic.Elements;
+using CrystalMoon.Systems.ScreenSystems;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using CrystalMoon.Registries;
+using CrystalMoon.Content.MoonlightMagic.Movements;
+using CrystalMoon.Systems.Particles;
+using CrystalMoon.Visual.Particles;
+using System.Threading;
+
+namespace CrystalMoon.Content.MoonlightMagic.Enchantments.Hex
+{
+    internal class TrailerEnchantment : BaseEnchantment
+    {
+        
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            time = 15;
+        }
+    
+        public override void AI()
+        {
+            base.AI();
+
+            //Count up
+            Countertimer++;
+
+            //If greater than time then start homing, we'll just swap the movement type of the projectile
+
+
+            if (Countertimer < time)
+            {
+                MagicProj.TrailLength += 1;
+            }
+        }
+
+        public override float GetStaffManaModifier()
+        {
+            return 0.2f;
+        }
+
+        public override int GetElementType()
+        {
+            return ModContent.ItemType<HexElement>();
+        }
+
+
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+
+            return true;
+        }
+
+        public override void SpecialInventoryDraw(Item item, SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            base.SpecialInventoryDraw(item, spriteBatch, position, frame, drawColor, itemColor, origin, scale);
+            DrawHelper.DrawGlowInInventory(item, spriteBatch, position, ColorUtil.HexPurple);
+        }
+    }
+}
