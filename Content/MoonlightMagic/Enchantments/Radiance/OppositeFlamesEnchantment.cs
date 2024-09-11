c7131d2ac7877315db1b1cc55ae001e61951d171
Zenovia <calamityjewelstudios@gmail.com>
2024-09-11 16:54:03 -0400

    New reversing stuff

---
+++ Content/MoonlightMagic/Enchantments/Radiance/OppositeFlamesEnchantment.cs
@@ -0,0 +1,65 @@
+﻿using CrystalMoon.Content.MoonlightMagic.Elements;
+using CrystalMoon.Systems.ScreenSystems;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using CrystalMoon.Registries;
+using CrystalMoon.Content.MoonlightMagic.Movements;
+using CrystalMoon.Systems.Particles;
+using CrystalMoon.Visual.Particles;
+
+namespace CrystalMoon.Content.MoonlightMagic.Enchantments.Radiance
+{
+    internal class OppositeFlamesEnchantment : BaseEnchantment
+    {
+        
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            time = 30;
+        }
+
+        public override void AI()
+        {
+            base.AI();
+
+            //Count up
+            Countertimer++;
+
+            //If greater than time then start homing, we'll just swap the movement type of the projectile
+            if (Countertimer == time)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Vector2 spawnPoint = Projectile.Center + Main.rand.NextVector2Circular(8, 8);
+                    Vector2 velocity = Main.rand.NextVector2Circular(8, 8);
+                    Particle.NewParticle<SparkleHexParticle>(spawnPoint, velocity, Color.White);
+                }
+
+                Projectile.velocity *= -1;
+            }
+        }
+
+        public override float GetStaffManaModifier()
+        {
+            return 0.2f;
+        }
+
+        public override int GetElementType()
+        {
+            return ModContent.ItemType<RadianceElement>();
+        }
+
+        public override void SpecialInventoryDraw(Item item, SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            base.SpecialInventoryDraw(item, spriteBatch, position, frame, drawColor, itemColor, origin, scale);
+            DrawHelper.DrawGlowInInventory(item, spriteBatch, position, ColorUtil.RadianceYellow);
+        }
+    }
+}
