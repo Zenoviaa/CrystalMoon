14bb20d578c5584ff76160d35696023e82b42a13
Zenovia <calamityjewelstudios@gmail.com>
2024-09-11 18:14:29 -0400

    new staff

---
+++ Content/MoonlightMagic/Weapons/ChalicemanSepter.cs
@@ -0,0 +1,30 @@
+﻿using CrystalMoon.Content.MoonlightMagic.Forms;
+
+namespace CrystalMoon.Content.MoonlightMagic.Weapons
+{
+    internal class ChalicemanSepter : BaseStaff
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.damage = 28;
+            Item.shootSpeed = 14;
+            Item.useTime = 20;
+            Item.useAnimation = 40;
+            Size = 10;
+            TrailLength = 22;
+            Form = FormRegistry.Triangle.Value;
+        }
+
+
+        public override int GetNormalSlotCount()
+        {
+            return 2;
+        }
+
+        public override int GetTimedSlotCount()
+        {
+            return 4;
+        }
+    }
+}
