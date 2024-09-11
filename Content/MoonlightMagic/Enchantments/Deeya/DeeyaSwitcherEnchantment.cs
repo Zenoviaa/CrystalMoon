e858c148a7125f5fecf73827a446b75e8aeb55e9
Zenovia <calamityjewelstudios@gmail.com>
2024-09-10 23:57:46 -0400

    guh

---
+++ Content/MoonlightMagic/Enchantments/Deeya/DeeyaSwitcherEnchantment.cs
@@ -0,0 +1,73 @@
+﻿using CrystalMoon.Content.MoonlightMagic.Elements;
+using CrystalMoon.Systems.ScreenSystems;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using CrystalMoon.Registries;
+using CrystalMoon.Content.MoonlightMagic.Movements;
+using CrystalMoon.Systems.Particles;
+using CrystalMoon.Visual.Particles;
+
+namespace CrystalMoon.Content.MoonlightMagic.Enchantments.Deeya
+{
+    internal class DeeyaSwitcherEnchantment : BaseEnchantment
+    {
+        private int _timer;
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            time = 45;
+        }
+
+        public override void AI()
+        {
+            base.AI();
+
+            //Count up
+            _timer++;
+
+            //If greater than time then start homing, we'll just swap the movement type of the projectile
+            if (_timer == time)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Vector2 spawnPoint = Projectile.Center + Main.rand.NextVector2Circular(8, 8);
+                    Vector2 velocity = Main.rand.NextVector2Circular(8, 8);
+                    Particle.NewParticle<GlowParticle>(spawnPoint, velocity, Color.White);
+                }
+               
+                MagicProj.PrimaryElement = new DeeyaElement();
+
+            }
+        }
+
+        public override float GetStaffManaModifier()
+        {
+            return 0.2f;
+        }
+
+        public override int GetElementType()
+        {
+            return ModContent.ItemType<DeeyaElement>();
+        }
+
+
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+           // Projectile.velocity += (Projectile.velocity.SafeNormalize(Vector2.Zero) * 4).RotatedBy(MathHelper.ToRadians(15 * MathF.Sin(_timer)));
+            return true;
+        }
+
+        public override void SpecialInventoryDraw(Item item, SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            base.SpecialInventoryDraw(item, spriteBatch, position, frame, drawColor, itemColor, origin, scale);
+            DrawHelper.DrawGlowInInventory(item, spriteBatch, position, ColorUtil.HexPurple);
+        }
+    }
+}
