b96e1395f9970761401c90e9a00a6cffed2e6901
Zenovia <calamityjewelstudios@gmail.com>
2024-09-11 00:17:30 -0400

    Spinback enchantment

---
+++ Content/MoonlightMagic/Enchantments/Guuts/SpinbackEnchantment.cs
@@ -0,0 +1,63 @@
+﻿using CrystalMoon.Content.Bases;
+using CrystalMoon.Content.MoonlightMagic.Elements;
+using CrystalMoon.Content.MoonlightMagic.Enchantments.Radiance;
+using CrystalMoon.Registries;
+using CrystalMoon.Systems.MiscellaneousMath;
+using CrystalMoon.Systems.ScreenSystems;
+using CrystalMoon.Systems.Shaders;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.UI.Chat;
+
+namespace CrystalMoon.Content.MoonlightMagic.Enchantments.Guuts
+{
+    internal class SpinbackEnchantment : BaseEnchantment
+    {
+       
+        public override float GetStaffManaModifier()
+        {
+            return 0.3f;
+        }
+
+        public override int GetElementType()
+        {
+            return ModContent.ItemType<GuutElement>();
+        }
+
+
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+
+            return true;
+        }
+
+        public override void SpecialInventoryDraw(Item item, SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            base.SpecialInventoryDraw(item, spriteBatch, position, frame, drawColor, itemColor, origin, scale);
+            DrawHelper.DrawGlowInInventory(item, spriteBatch, position, Color.LightGray);
+        }
+
+        public override void SetMagicDefaults()
+        {
+            Projectile.knockBack *= 2;
+          
+        }
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            base.OnHitNPC(target, hit, damageDone);
+
+            //Spawn the explosion
+            
+            
+
+        }
+
+       
+
+    }
+
+    
+}
