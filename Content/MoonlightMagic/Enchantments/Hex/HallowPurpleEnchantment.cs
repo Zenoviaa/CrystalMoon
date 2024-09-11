59841750956ff113565510f029c04def26e0f0d2
Zenovia <calamityjewelstudios@gmail.com>
2024-09-10 20:00:42 -0400

    f

---
+++ Content/MoonlightMagic/Enchantments/Hex/HallowPurpleEnchantment.cs
@@ -0,0 +1,242 @@
+﻿using CrystalMoon.Content.Bases;
+using CrystalMoon.Content.MoonlightMagic.Elements;
+using CrystalMoon.Content.MoonlightMagic.Enchantments.Radiance;
+using CrystalMoon.Content.MoonlightMagic.Movements;
+using CrystalMoon.Registries;
+using CrystalMoon.Systems.MiscellaneousMath;
+using CrystalMoon.Systems.Particles;
+using CrystalMoon.Systems.ScreenSystems;
+using CrystalMoon.Systems.Shaders;
+using CrystalMoon.Visual.Particles;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.UI.Chat;
+
+namespace CrystalMoon.Content.MoonlightMagic.Enchantments.Hex
+{
+    internal class HallowPurpleEnchantment : BaseEnchantment
+    {
+        public override float GetStaffManaModifier()
+        {
+            return 1f;
+        }
+
+        private int _timer;
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            time = 60;
+        }
+
+        public override void AI()
+        {
+            base.AI();
+
+            //Count up
+            _timer++;
+
+            //If greater than time then start homing, we'll just swap the movement type of the projectile
+            if (_timer == time)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Vector2 spawnPoint = Projectile.Center + Main.rand.NextVector2Circular(8, 8);
+                    Vector2 velocity = Main.rand.NextVector2Circular(8, 8);
+                    Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity / 2, ModContent.ProjectileType<HallowPurpleEnchantmentExplosion>(),
+                    Projectile.damage * 2, Projectile.knockBack, Projectile.owner);
+
+
+                    Particle.NewParticle<SparkleHexParticle>(spawnPoint, velocity, Color.White);
+
+                }
+
+              
+            }
+        }
+        public override int GetElementType()
+        {
+            return ModContent.ItemType<HexElement>();
+        }
+
+
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+
+            return true;
+        }
+
+        public override void SpecialInventoryDraw(Item item, SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            base.SpecialInventoryDraw(item, spriteBatch, position, frame, drawColor, itemColor, origin, scale);
+            DrawHelper.DrawGlowInInventory(item, spriteBatch, position, Color.LightPink);
+        }
+
+      
+    }
+
+    internal class HallowPurpleEnchantmentExplosion : BaseExplosionProjectile
+    {
+        int trailMode;
+        int rStart = 4;
+        int rEnd = 428;
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Projectile.width = 48;
+            Projectile.height = 48;
+            Projectile.friendly = true;
+            Projectile.penetrate = -1;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 20;
+            Projectile.timeLeft = 240;
+            rStart = Main.rand.Next(60, 64);
+            rEnd = Main.rand.Next(400, 428);
+        }
+       
+        public override void AI()
+        {
+           
+
+
+            for (int i = 0; i < Main.maxNPCs; i++)
+            {
+                NPC npc = Main.npc[i];
+
+                if (npc.active && !npc.friendly && !npc.boss)
+                {
+                    float distance = Vector2.Distance(Projectile.Center, npc.Center);
+                    if (distance <= 400)
+                    {
+                        Vector2 direction = npc.Center - Projectile.Center;
+                        direction.Normalize();
+                        npc.velocity -= direction * 0.5f;
+                    }
+                }
+            }
+        }
+        public override void OnKill(int timeLeft)
+        {
+            SoundEngine.PlaySound(SoundID.DD2_EtherianPortalOpen, Projectile.position);
+            Projectile.ownerHitCheck = true;
+
+            int radius = 250;
+
+            // Damage enemies within the splash radius
+            for (int i = 0; i < Main.npc.Length; i++)
+            {
+                NPC target = Main.npc[i];
+                if (target.active && !target.friendly && Vector2.Distance(Projectile.Center, target.Center) < radius)
+                {
+                    int damage = Projectile.damage * 2;
+                    target.SimpleStrikeNPC(damage: 12, 0);
+                }
+            }
+
+            for (int i = 0; i < 50; i++)
+            {
+                Vector2 spawnPoint = Projectile.Center + Main.rand.NextVector2Circular(8, 8);
+                Vector2 speed = Main.rand.NextVector2CircularEdge(4f, 4f);
+                Particle.NewParticle<SparkleHexParticle>(spawnPoint, speed, Color.White);
+            }
+        }
+
+        protected override float BeamWidthFunction(float p)
+        {
+            //How wide the trail is going to be
+            float trailWidth = MathHelper.Lerp(64, 8, p);
+            float fadeWidth = MathHelper.Lerp(0, trailWidth, Easing.SpikeOutCirc(p)) * Main.rand.NextFloat(0.75f, 1.0f);
+            return fadeWidth;
+        }
+
+        protected override Color ColorFunction(float p)
+        {
+            //Main color of the beam
+            Color c;
+            switch (trailMode)
+            {
+                default:
+                case 0:
+                    c = Color.Lerp(Color.White, new Color(147, 72, 121) * 0.5f, p);
+                    break;
+                case 1:
+                    c = Color.Lerp(Color.White, new Color(147, 72, 121) * 0f, p);
+                    break;
+                case 2:
+                    c = Color.White;
+                    c.A = 0;
+                    break;
+            }
+
+            return c;
+        }
+
+        protected override float RadiusFunction(float p)
+        {
+            //How large the circle is going to be
+            return MathHelper.Lerp(rStart, rEnd, Easing.OutQuint(p));
+        }
+
+        protected override BaseShader ReadyShader()
+        {
+            var shader = SimpleTrailShader.Instance;
+
+            //Main trailing texture
+            shader.TrailingTexture = TextureRegistry.GlowTrail;
+
+            //Blends with the main texture
+            shader.SecondaryTrailingTexture = TextureRegistry.GlowTrail;
+
+            //Used for blending the trail colors
+            //Set it to any noise texture
+            shader.TertiaryTrailingTexture = TextureRegistry.CrystalTrail;
+            shader.PrimaryColor = Color.DarkGoldenrod;
+            shader.SecondaryColor = Color.Purple;
+            shader.Speed = 20;
+
+            //Alpha Blend/Additive
+            shader.BlendState = BlendState.Additive;
+            shader.SamplerState = SamplerState.PointWrap;
+            shader.FillShape = true;
+            return shader;
+        }
+
+        private void DrawMainShader()
+        {
+            //Trail
+            trailMode = 0;
+            var shader = MagicHexShader.Instance;
+
+            shader.PrimaryTexture = TextureRegistry.GlowTrail;
+            shader.NoiseTexture = TextureRegistry.NoiseTextureCloudsSmall;
+            shader.PrimaryColor = new Color(195, 158, 255);
+            shader.NoiseColor = new Color(78, 76, 180);//new Color(78, 76, 180);
+            shader.OutlineColor = Color.White;
+            shader.BlendState = BlendState.Additive;
+            shader.SamplerState = SamplerState.PointWrap;
+            shader.Speed = 5.2f;
+            shader.Distortion = 0.1f;
+
+            //This just applis the shader changes
+            TrailDrawer.Draw(Main.spriteBatch, _circlePos, Projectile.oldRot, ColorFunction, WidthFunction, shader, offset: Projectile.Size / 2);
+        }
+
+       
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            DrawMainShader();
+            // DrawOutlineShader();
+            return false ;
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            base.OnHitNPC(target, hit, damageDone);
+          //  target.AddBuff(BuffID.OnFire, 90);
+        }
+    }
+}
