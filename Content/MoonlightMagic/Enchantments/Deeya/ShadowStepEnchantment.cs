cda0caad36ac2c0f1e3fb15a42284814aed6b2fa
Scolon <49799704+sscolon@users.noreply.github.com>
2024-09-11 00:24:37 -0400

    shadow step

---
+++ Content/MoonlightMagic/Enchantments/Deeya/ShadowStepEnchantment.cs
@@ -0,0 +1,37 @@
+﻿using CrystalMoon.Content.Bases;
+using CrystalMoon.Content.MoonlightMagic.Elements;
+using CrystalMoon.Content.MoonlightMagic.Enchantments.Radiance;
+using CrystalMoon.Registries;
+using CrystalMoon.Systems.MiscellaneousMath;
+using CrystalMoon.Systems.ScreenSystems;
+using CrystalMoon.Systems.Shaders;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.UI.Chat;
+
+namespace CrystalMoon.Content.MoonlightMagic.Enchantments.Deeya
+{
+    internal class ShadowStepEnchantment : BaseEnchantment
+    {
+        public override float GetStaffManaModifier()
+        {
+            return 0.12f;
+        }
+
+        public override int GetElementType()
+        {
+            return ModContent.ItemType<DeeyaElement>();
+        }
+
+        public override void SetMagicDefaults()
+        {
+            float range = MagicProj.Size / 8f;
+            Projectile.velocity = Projectile.velocity.RotatedByRandom(MathHelper.PiOver4/2);
+            Projectile.position.X += Main.rand.NextFloat(-range, range);
+            Projectile.position.Y += Main.rand.NextFloat(-range, range);
+        }
+    }
+}
