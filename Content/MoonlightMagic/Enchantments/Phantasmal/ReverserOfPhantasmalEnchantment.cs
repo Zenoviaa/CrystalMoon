ef442ed3a62f226eab398eaded201f9e053472f0
Zenovia <calamityjewelstudios@gmail.com>
2024-09-11 16:20:12 -0400

    finish reversers

---
+++ Content/MoonlightMagic/Enchantments/Phantasmal/ReverserOfPhantasmalEnchantment.cs
@@ -0,0 +1,74 @@
+﻿using CrystalMoon.Content.MoonlightMagic.Elements;
+using CrystalMoon.Systems.ScreenSystems;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using CrystalMoon.Registries;
+using CrystalMoon.Content.MoonlightMagic.Movements;
+using CrystalMoon.Systems.Particles;
+using CrystalMoon.Visual.Particles;
+using System.Threading;
+
+namespace CrystalMoon.Content.MoonlightMagic.Enchantments.Phantasmal
+{
+    internal class ReverserOfPhantasmalEnchantment : BaseEnchantment
+    {
+        
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            time = 200;
+        }
+        
+        public override void AI()
+        {
+            base.AI();
+
+            //Count up
+            Countertimer++;
+
+            //If greater than time then start homing, we'll just swap the movement type of the projectile
+
+                foreach (var enchantment in MagicProj.Enchantments)
+                {
+                    //do a thing here
+                    if (enchantment.Countertimer > enchantment.time)
+                    {
+                        enchantment.Countertimer = 0;
+                    }
+
+                }
+            
+
+        }
+
+        public override float GetStaffManaModifier()
+        {
+            return 1f;
+        }
+
+        public override int GetElementType()
+        {
+            return ModContent.ItemType <PhantasmalElement>();
+        }
+
+
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+
+            return true;
+        }
+
+        public override void SpecialInventoryDraw(Item item, SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            base.SpecialInventoryDraw(item, spriteBatch, position, frame, drawColor, itemColor, origin, scale);
+            DrawHelper.DrawGlowInInventory(item, spriteBatch, position, ColorUtil.PhantasmalGreen);
+        }
+    }
+}
