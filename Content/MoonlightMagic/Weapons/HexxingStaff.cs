ded633cd6cb65d85d56a632b3960cc4d3568ca64
Zenovia <calamityjewelstudios@gmail.com>
2024-09-18 14:13:34 -0400

    Hexxing Staff

---
+++ Content/MoonlightMagic/Weapons/HexxingStaff.cs
@@ -0,0 +1,30 @@
+﻿using CrystalMoon.Content.MoonlightMagic.Forms;
+
+namespace CrystalMoon.Content.MoonlightMagic.Weapons
+{
+    internal class HexxingStaff : BaseStaff
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.damage = 10;
+            Item.shootSpeed = 16;
+            Item.useTime = 23;
+            Item.useAnimation = 23;
+            Size = 8;
+            TrailLength = 30;
+            Form = FormRegistry.Snowglobe.Value;
+        }
+
+
+        public override int GetNormalSlotCount()
+        {
+            return 3;
+        }
+
+        public override int GetTimedSlotCount()
+        {
+            return 0;
+        }
+    }
+}
