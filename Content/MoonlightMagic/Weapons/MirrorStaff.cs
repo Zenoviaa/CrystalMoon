ff9f9deed5324b43a8f7cfff7ac18af849f330fc
Zenovia <calamityjewelstudios@gmail.com>
2024-10-10 23:19:08 -0400

    New staff

---
+++ Content/MoonlightMagic/Weapons/MirrorStaff.cs
@@ -0,0 +1,30 @@
+﻿using CrystalMoon.Content.MoonlightMagic.Forms;
+
+namespace CrystalMoon.Content.MoonlightMagic.Weapons
+{
+    internal class MirrorStaff : BaseStaff
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.damage = 35;
+            Item.shootSpeed = 16;
+            Item.useTime = 40;
+            Item.useAnimation = 40;
+            Size = 8;
+            TrailLength = 50;
+            Form = FormRegistry.Snowglobe.Value;
+        }
+
+
+        public override int GetNormalSlotCount()
+        {
+            return 3;
+        }
+
+        public override int GetTimedSlotCount()
+        {
+            return 4;
+        }
+    }
+}
