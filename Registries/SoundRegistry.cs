8e81c6beba61d38baed41f589cd1e22ee66f5d6c
Zenovia <calamityjewelstudios@gmail.com>
2024-10-11 19:01:55 -0400

    h

--- Registries/SoundRegistry.cs
+++ Registries/SoundRegistry.cs
@@ -7,16 +7,26 @@ namespace CrystalMoon.Registries
         public static string RootAssetPath => "CrystalMoon/Assets/Sounds/";
 
         //Example how to add
-       // public static SoundStyle StormDragonLightningRain => new SoundStyle($"{RootAssetPath}StormDragon_LightingZap");
+        // public static SoundStyle StormDragonLightningRain => new SoundStyle($"{RootAssetPath}StormDragon_LightingZap");
 
+        public static SoundStyle NSwordSlash1 => new SoundStyle($"{RootAssetPath}NormalSwordSlash1");
 
+        public static SoundStyle NSwordSlash2 => new SoundStyle($"{RootAssetPath}NormalSwordSlash2");
+
+        public static SoundStyle NSwordHit1 => new SoundStyle($"{RootAssetPath}NormalSwordHit1");
+
+        public static SoundStyle NSwordSpin1 => new SoundStyle($"{RootAssetPath}SwordSpin1");
+
+        public static SoundStyle CurveSwordSlash1 => new SoundStyle($"{RootAssetPath}CurveSwordSlash1");
         /*
         public static SoundStyle BowCharge => new SoundStyle($"{RootAssetPath}BowCharge",
             variantSuffixesStart: 1,
             numVariants: 2);
+
         public static SoundStyle BowShoot => new SoundStyle($"{RootAssetPath}BowShot",
             variantSuffixesStart: 1,
             numVariants: 2);
         */
+
     }
 }
