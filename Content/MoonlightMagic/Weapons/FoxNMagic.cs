3097943ff20769df1da7d7e00b0efe39ab3b3506
Zenovia <calamityjewelstudios@gmail.com>
2024-10-18 01:48:23 -0400

    new staff

---
+++ Content/MoonlightMagic/Weapons/FoxNMagic.cs
@@ -0,0 +1,30 @@
+﻿using CrystalMoon.Content.MoonlightMagic.Forms;
+
+namespace CrystalMoon.Content.MoonlightMagic.Weapons
+{
+    internal class FoxNMagic : BaseStaff
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.damage = 39;
+            Item.shootSpeed = 13;
+            Item.useTime = 30;
+            Item.useAnimation = 60;
+            Size = 12;
+            TrailLength = 20;
+            Form = FormRegistry.Swirl.Value;
+        }
+
+
+        public override int GetNormalSlotCount()
+        {
+            return 5;
+        }
+
+        public override int GetTimedSlotCount()
+        {
+            return 4;
+        }
+    }
+}
