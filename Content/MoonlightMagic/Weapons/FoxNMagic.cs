9fa0fa38ac7b611362afc08491f507b45c1833e8
Zenovia <calamityjewelstudios@gmail.com>
2024-10-18 14:34:51 -0400

    f

--- Content/MoonlightMagic/Weapons/FoxNMagic.cs
+++ Content/MoonlightMagic/Weapons/FoxNMagic.cs
@@ -11,7 +11,7 @@ namespace CrystalMoon.Content.MoonlightMagic.Weapons
             Item.shootSpeed = 13;
             Item.useTime = 30;
             Item.useAnimation = 60;
-            Size = 12;
+            Size = 8;
             TrailLength = 20;
             Form = FormRegistry.Swirl.Value;
         }
