1af427c1153b32a3d0961829366960f7b6037564
Scolon <scoloned@gmail.com>
2024-10-18 14:23:49 -0400

    lightning and prime magic element

--- Systems/Shaders/LightningTrail.cs
+++ Systems/Shaders/LightningTrail.cs
@@ -21,8 +21,15 @@ namespace CrystalMoon.Systems.Shaders
             offsets[0] = Vector2.Zero;
             for (int i = 1; i < offsets.Length - 1; i++)
             {
+
                 Vector2 prevPosition = oldPos[i - 1];
                 Vector2 nextPosition = oldPos[i + 1];
+                if (prevPosition == Vector2.Zero || nextPosition == Vector2.Zero)
+                {
+                    offsets[i] = Vector2.Zero;
+                    continue;
+                }
+
                 Vector2 normalDir = (prevPosition - nextPosition).SafeNormalize(Vector2.One).RotatedBy(MathHelper.PiOver2);
                 float length = Main.rand.NextFromList(-1, 1) * Main.rand.NextFloat(LightningRandomOffsetRange, LightningRandomOffsetRange);
                 offsets[i] = (normalDir * length) + Main.rand.NextVector2Circular(LightningRandomExpand, LightningRandomExpand);
@@ -36,7 +43,7 @@ namespace CrystalMoon.Systems.Shaders
             if (_offsets == null)
                 return;
             Vector2[] lightningTrailPos = new Vector2[oldPos.Length];
-            for(int i = 0; i < lightningTrailPos.Length; i++)
+            for(int i = 0; i < lightningTrailPos.Length && i < _offsets.Length; i++)
             {
                 lightningTrailPos[i] = oldPos[i] + _offsets[i];
             }
